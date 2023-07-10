b4d63a25adf99f7cbb508be46c57b44070c74e52
samuelshola777 <samuelshola14@gmail.com>
2023-07-10 16:02:51 +0100

    learning stream

--- src/StreamLessons/ImperativeApproach.java
+++ src/StreamLessons/ImperativeApproach.java
@@ -11,8 +11,8 @@ public class ImperativeApproach {
                 new Person("tinuAde",Gender.FEMALE, "23"),
                 new Person("amirah",Gender.FEMALE,"16"),
                 new Person("tolu",Gender.FEMALE,"20"),
-                new Person("shola",Gender.MALE,"27")
-
+                new Person("shola",Gender.MALE,"27"),
+                new Person("glory", Gender.FEMALE,"23")
         );
 }
 
