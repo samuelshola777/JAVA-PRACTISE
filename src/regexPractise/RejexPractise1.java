92074164f08cd9d424246381ac9425f2ff77f2c4
samuelshola777 <samuelshola14@gmail.com>
2023-08-06 17:35:30 +0100

    best practise

--- src/regexPractise/RejexPractise1.java
+++ src/regexPractise/RejexPractise1.java
@@ -10,10 +10,16 @@ public class RejexPractise1 {
     //todo in the example below measn we're expecting a patternof agent and 3 number
 
 String goat = "agent 007";
-        Pattern pattern = Pattern.compile("agent \\d\\d\\d");
-        Matcher matcher = pattern.matcher("agent 007");
-        boolean found = matcher.find();
-        System.out.println(found);
+//        Pattern pattern = Pattern.compile("agent \\d\\d\\d");
+//        Matcher matcher = pattern.matcher("agent 007");
+//        boolean found = matcher.find();
+//        System.out.println(found);
+         Pattern pattern1 = Pattern.compile("agent \\d{3}");
+        Matcher matcher1 = pattern1.matcher("agent 007");
+        boolean found1 = matcher1.find();
+        System.out.println(found1);
+
+
     }
 
 }
